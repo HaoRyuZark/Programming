6550eb109c21f62394b8bca738ae3f9b0846d004
miguel <miguelalemania15@gmail.com>
2025-10-02 21:31:00 +0200

    Gaaaa (Thu Oct  2 09:31:00 PM CEST 2025)

--- DSA/Algorithms/Processes.c
+++ DSA/Algorithms/Processes.c
@@ -7,85 +7,133 @@
 
 #define PROCESS_NUM 10
 
+// Example Programm of some of the multiproccesing functionalities in C 
+
 int main(int argc, char* argv[]) {
-    int pids[PROCESS_NUM];  //arr of processes
+    
+    int pids[PROCESS_NUM];  //arr of processes ids
+    
+    // Pipes are a way of comunication between processes. The function pipe() return two file descriptors to 
+    // reference a region in memory to write and read data to (int[2]).
+    
     int pipes[PROCESS_NUM + 1][2]; //pipes 10 + 1 because of the parent process
     int i;
-    for (i = 0; i < PROCESS_NUM + 1; i++) { //creating pipes
+    
+    // Creating pipes
+    for (i = 0; i < PROCESS_NUM + 1; i++) {
         if (pipe(pipes[i]) == -1) {
             printf("Error with creating pipe\n");
             return 1;
         }
     }
-
-    for (i = 0; i < PROCESS_NUM; i++) {  //creating processes
+    
+    // Creating different processes which are clones of the current process
+    for (i = 0; i < PROCESS_NUM; i++) {
+        
         pids[i] = fork();
+
+        // ----------------------- The cloned processes start executing from here ---------------- 
+        
         if (pids[i] == -1) {
             printf("Error with creating process\n");
             return 2;
         }
-        //Childs
+        
+
+        // If the id is not the id of the parent process then 
+        // Take a value from one process increment its value and finally give it to the next process    
         if (pids[i] == 0) {
-            // Child process
+
             int j;
+           
             for (j = 0; j < PROCESS_NUM + 1; j++) {
                 if (i != j) {
-                    close(pipes[j][0]); //close all read read pipes of the other processes except the current
+                    close(pipes[j][0]);         //close all read-pipes of the other processes except the current
                 }
-                if (i + 1 != j) {       //the same but with read
+                if (i + 1 != j) {                   //the same but with the write except for the next process
                     close(pipes[j][1]);
                 }
             }
+            
             int x;
-            if (read(pipes[i][0], &x, sizeof(int)) == -1) { //read from the current process
+
+            if (read(pipes[i][0], &x, sizeof(int)) == -1) {    //read from the current process
                 printf("Error at reading\n");
                 return 3;
             }
+            
             printf("(%d) Got %d\n", i, x);
+            
             x++;
+            
             if (write(pipes[i + 1][1], &x, sizeof(int)) == -1) { //write to the next process
                 printf("Error at writing\n");
                 return 4;
             }
+            
             printf("(%d) Sent %d\n", i, x);
-            close(pipes[i][0]);  //close read end
+            
+            close(pipes[i][0]);      //close read end of the current process 
+            
             close(pipes[i + 1][1]);  //close write end of the next process
+            
             return 0;
         }
     }
 
     // Main process
     int j;
+    
+
     for (j = 0; j < PROCESS_NUM + 1; j++) {
+
         if (j != PROCESS_NUM) {
-            close(pipes[j][0]); //closes all other pipes execept the last read
+            close(pipes[j][0]); //closes all read pipes execept the last read
         }
-        if (j != 0) {  //except the write for the parent process
+        
+        if (j != 0) {               //close all write pipes except for the parent process
             close(pipes[j][1]);
         }
     }
+   
+    // Procedure for sending the first value 
+
     int y = 5;
+
     printf("Main process sent %d\n", y);
-    if (write(pipes[0][1], &y, sizeof(int)) == -1) { //write 5 to the pipe
+    
+    if (write(pipes[0][1], &y, sizeof(int)) == -1) { //write value to the next process
         printf("Error at writing\n");
         return 4;
     }
+    
+    // Read from the last process
     if (read(pipes[PROCESS_NUM][0], &y, sizeof(int)) == -1) { //read from the last process
         printf("Error at reading\n");
         return 3;
     }
+
     printf("The final result is %d\n", y);
+    
+    // Clean Up
     close(pipes[0][1]); //close last pipes
     close(pipes[PROCESS_NUM][0]);
+    
+    //Wait for all processes to do their work. Wait is called after read because the parent 
+    //process must first retrieve the result from the pipeline. Also, it has to signalize that it is 
+    //expecting some result of the last process because other wise the last child processes will be never 
+    //terminate properly because it has no way of sending the data
 
     for (i = 0; i < PROCESS_NUM; i++) { //wait for all processes
         wait(NULL);
     }
 
-    //Other stuff
+    // --------------  Creating other processes which are not clones  -----------------------
     int omega;
+   
     printf("Choose 1 or 0? ");
     scanf("%d",&omega);
+   
     if(omega == 1){
        
         // Execute external processes
--- Projects/Multithreaded_Server/Thread_Pool.c
+++ Projects/Multithreaded_Server/Thread_Pool.c
@@ -3,129 +3,160 @@
 #include <threads.h>
 #include <unistd.h>
 #include <string.h>
+#include <time.h>
 
 #define THREAD_NUM 10
 
+// Global mutex and condition variable for synchronizing access to the queue
 cnd_t condQueue;
 mtx_t mutexQueue;
+int done = 0; // Flag to indicate when no more tasks will be enqueued
 
+// Task representation
 typedef struct Task {
-    int(*task_function)(int, int);
+
+    int (*task_function)(int, int);
     int arg1, arg2;
     struct Task* next;
 
 } Task;
 
+// Queue structure for storing tasks
 typedef struct Queue {
+
     Task* first;
     Task* last;
     int length;
 
 } Queue;
 
+// Create a new task
 Task* task_constructor(int arg1, int arg2, int(*task_function)(int, int)) {
+
     Task* task = (Task*)malloc(sizeof(Task));
-    
     task->arg1 = arg1;
     task->arg2 = arg2;
     task->task_function = task_function;
     task->next = NULL;
-
+   
     return task;
 }
 
+// Enqueue a task into the queue (thread-safe)
 void enqueue(Queue* self, Task* task) {
+
     mtx_lock(&mutexQueue);
-    if(!self->first) {
+   
+    if (!self->first) {
         self->first = task;
         self->last = task;
     } else {
         self->last->next = task;
         self->last = task;
     }
-    self->length += 1;
+   
+    self->length++;
+   
     mtx_unlock(&mutexQueue);
-    cnd_signal(&condQueue);
+    cnd_signal(&condQueue); // Wake up one waiting thread
 }
 
-void dequeue(Queue* self, int* result) {
-    if(!self->first) {
-        return;
+// Dequeue and execute a task (thread-safe)
+int dequeue(Queue* self) {
+
+    mtx_lock(&mutexQueue);
+   
+    while (self->length == 0 && !done) {
+        cnd_wait(&condQueue, &mutexQueue); // Wait until new task arrives
     }
-    
+
+    if (done && self->length == 0) {
+        mtx_unlock(&mutexQueue);
+        return -1; // Signal termination
+    }
+
     Task* current = self->first;
     self->first = self->first->next;
-    if(!self->first) {
+   
+    if (!self->first) {
         self->last = NULL;
     }
-    self->length -= 1;
-    *result = current->task_function(current->arg1, current->arg2);
+   
+    self->length--;
+
+    mtx_unlock(&mutexQueue);
+
+    // Execute task
+    int result = current->task_function(current->arg1, current->arg2);
+   
+    printf("The sum of %d and %d is %d\n", current->arg1, current->arg2, result);
     free(current);
+
+    return result;
 }
 
+// Example task functions
 int sum(int a, int b) {
     return a + b;
 }
-
 int product(int a, int b) {
     return a * b;
 }
 
-Queue queue = {
-    .first = NULL,
-    .last = NULL,
-    .length = 0
-};
+// Global queue
+Queue queue = { .first = NULL, .last = NULL, .length = 0 };
 
+// Thread function
 int startThread(void* args) {
-
-    int* result = (int*)(args);
-
-    while(1) {
-        Task* task;
-
-        mtx_lock(&mutexQueue);
-        while(queue.length == 0) {
-            cnd_wait(&condQueue, &mutexQueue);
-
-        }
-
-        task = queue.first;
-        mtx_unlock(&mutexQueue);
-        dequeue(&queue, result);
+   
+    (void)args; // Unused
+   
+    while (1) {
+        int result = dequeue(&queue);
+        if (result == -1) break; // Stop thread
     }
-
     return 0;
 }
 
-
-int main(int argc, char* argv[]) {
-
+int main() {
+   
     thrd_t th[THREAD_NUM];
-    mtx_init(&mutexQueue, NULL);
+   
+    mtx_init(&mutexQueue, mtx_plain);
     cnd_init(&condQueue);
 
-    int i;
-    int results[THREAD_NUM];
-    for(i = 0; i < THREAD_NUM; i++){
-        th[i] = thrd_create(&th[i], &startThread, (void*)&results[i]);
+    srand(time(NULL));
+
+    // Create threads
+   
+    for (int i = 0; i < THREAD_NUM; i++) {
+        thrd_create(&th[i], startThread, NULL);
     }
 
-    srand(time(NULL));
-    for(i = 0; i < THREAD_NUM; i++) {
+    // Enqueue random tasks
+    for (int i = 0; i < THREAD_NUM; i++) {
+        
         int a = rand() % 100;
         int b = rand() % 100;
-
         Task* t = task_constructor(a, b, sum);
-
         enqueue(&queue, t);
     }
 
-    for(i = 0; i < THREAD_NUM; i++){
+    // Signal threads to finish
+    mtx_lock(&mutexQueue);
+   
+    done = 1;
+    
+    mtx_unlock(&mutexQueue);
+    cnd_broadcast(&condQueue); // Wake up all threads
+
+    // Join threads
+    for (int i = 0; i < THREAD_NUM; i++) {
         thrd_join(th[i], NULL);
     }
 
     mtx_destroy(&mutexQueue);
     cnd_destroy(&condQueue);
+
     return 0;
 }
